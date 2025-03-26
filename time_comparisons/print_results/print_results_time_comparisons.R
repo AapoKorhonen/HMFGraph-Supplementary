0cbf207ff839e7468fe6ce18f2b62b7806e9c50c
Aapo Korhonen <aapo.korhonen23@gmail.com>
2025-03-26 17:32:36 +0200

    time comparisons

--- time_comparisons/print_results/print_results_time_comparisons.R
+++ time_comparisons/print_results/print_results_time_comparisons.R
@@ -53,38 +53,38 @@ benchmark_Result100_G_wishart
 
 load(file="time_comparisons/p_200/benchmark_Result200_HMFGraph.RData")
 benchmark_Result200_HMFGraph
-# Unit: milliseconds
-#  median : 299.5302
+# Unit: seconds
+#  median : 1.419247
 
 load(file="time_comparisons/p_200/benchmark_Result200_HMFGraph_P.RData")
 benchmark_Result200_HMFGraph_P
 # Unit: seconds
-#  median : 3.949834
+#  median : 11.82196
 
 load(file="time_comparisons/p_200/benchmark_Result200_HMFGraph_P_alpha.RData")
 benchmark_Result200_HMFGraph_P_alpha
 # Unit: seconds
-#  median : 5.314875
+#  median : 19.76574
 
 load(file="time_comparisons/p_200/benchmark_Result200_HMFGraph_P_alpha_WP.RData")
 benchmark_Result200_HMFGraph_P_alpha_WP
 # Unit: seconds
-#  median : 11.81925
+#  median : 71.06114
 
 load(file="time_comparisons/p_200/benchmark_Result200_HMFGraph_Gibbs.RData")
 benchmark_Result200_HMFGraph_Gibbs
 # Unit: seconds
-#  median : 14.37286
+#  median : 90.68634
 
 load(file="time_comparisons/p_200/benchmark_Result200_BGGM.RData")
 benchmark_Result200_BGGM
 # Unit: seconds
-#  median : 12.02836
+#  median : 88.54844
 
 load(file="time_comparisons/p_200/benchmark_Result200_G_wishart.RData")
 benchmark_Result200_G_wishart
 # Unit: seconds
-#  median : 16.69958
+#  median : 90.42888
 
 
 #=================================================================================
@@ -94,38 +94,38 @@ benchmark_Result200_G_wishart
 
 load(file="time_comparisons/p_300/benchmark_Result300_HMFGraph.RData")
 benchmark_Result300_HMFGraph
-# Unit: milliseconds
-#  median : 299.5302
+# Unit: seconds
+#  median : 5.501008
 
 load(file="time_comparisons/p_300/benchmark_Result300_HMFGraph_P.RData")
 benchmark_Result300_HMFGraph_P
 # Unit: seconds
-#  median : 3.949834
+#  median : 37.6393
 
 load(file="time_comparisons/p_300/benchmark_Result300_HMFGraph_P_alpha.RData")
 benchmark_Result300_HMFGraph_P_alpha
 # Unit: seconds
-#  median : 5.314875
+#  median : 53.61694
 
 load(file="time_comparisons/p_300/benchmark_Result300_HMFGraph_P_alpha_WP.RData")
 benchmark_Result300_HMFGraph_P_alpha_WP
 # Unit: seconds
-#  median : 11.81925
+#  median : 246.4868
 
 load(file="time_comparisons/p_300/benchmark_Result300_HMFGraph_Gibbs.RData")
 benchmark_Result300_HMFGraph_Gibbs
 # Unit: seconds
-#  median : 14.37286
+#  median : 305.1764
 
 load(file="time_comparisons/p_300/benchmark_Result300_BGGM.RData")
 benchmark_Result300_BGGM
 # Unit: seconds
-#  median : 12.02836
+#  median : 298.2529
 
 load(file="time_comparisons/p_300/benchmark_Result300_G_wishart.RData")
 benchmark_Result300_G_wishart
 # Unit: seconds
-#  median : 16.69958
+#  median : 488.8472
 
 #=================================================================================
 # p = 400
