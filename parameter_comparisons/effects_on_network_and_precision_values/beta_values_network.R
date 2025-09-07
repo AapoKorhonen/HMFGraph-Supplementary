5b2b6d9c63c8893a5352a3d9fd80530a0e7c5ba4
AapoKorhonen <aapo.korhonen23@gmail.com>
2025-09-07 23:28:41 +0300

    effect of beta on precision matrix

---
+++ parameter_comparisons/effects_on_network_and_precision_values/beta_values_network.R
@@ -0,0 +1,109 @@
+############################################################
+# Comparisons of networks received from GEM algortihm and Gibbs sampler.
+# Networks are selected based on credible intervals. 
+# 99 %, 95%, 90% and 80% CIs are used
+############################################################
+
+
+
+library(huge)
+library(HMFGraph)
+library(qgraph)
+
+set.seed(42)
+n <-100 # number of samples
+p <-20 # number of variables
+graph_data <- huge.generator(n = n, d =p, vis = TRUE, graph ="scale-free")
+data_R <-graph_data$data
+real_admat <-round(graph_data$omega,5)
+diag(real_admat) <- 0
+real_admat[abs(real_admat) > 0] <- 1
+
+
+#====================================
+# True network
+#====================================
+
+setEPS()
+postscript("beta_values_CI09_network.eps", width = 5, height = 15)   # koko oletuksena tuumina
+
+#par(mfrow=c(1,1))
+layout(matrix(c(1,2,3,4,5,6,7,8,9,10),5,2,byrow=TRUE))
+
+
+#qgraph::qgraph(real_admat, layout="circle", title="Scale-free network generated with huge R-package")
+
+
+
+
+#par(mfrow=c(4,2))
+
+
+
+#====================================
+# beta = 0.95
+#====================================
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha=0.80, beta=0.95)
+
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="alpha=0.80, beta=0.95")
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, beta=0.95)
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="CC-method, beta=0.95")
+
+
+#====================================
+# beta=0.90
+#====================================
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha=0.80, beta=0.90)
+
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="alpha=0.80, beta=0.90")
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, beta=0.90)
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="CC-method, beta=0.90")
+
+
+#====================================
+# beta=0.80
+#====================================
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha=0.80, beta=0.80)
+
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="alpha=0.80, beta=0.80")
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, beta=0.80)
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="CC-method, beta=0.80")
+
+
+#====================================
+# beta=0.70
+#====================================
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha=0.80, beta=0.70)
+
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="alpha=0.80, beta=0.70")
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, beta=0.70)
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="CC-method, beta=0.70")
+
+#====================================
+# beta=0.60
+#====================================
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha=0.80, beta=0.60)
+
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="alpha=0.80, beta=0.60")
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, beta=0.60)
+gem_network  <- HMFGraph::HMFGraph_GEM_CI(results_GEM, CI=0.90)
+qgraph::qgraph(gem_network$adjacency_matrix, layout="circle", title="CC-method, beta=0.60")
+
+
+dev.off()
---
+++ parameter_comparisons/effects_on_network_and_precision_values/beta_values_offdiagonals.R
@@ -0,0 +1,84 @@
+############################################################
+# Comparisons of values of off diagonal elements of estimated precision matrix with multiple different beta values
+# For all, alpha = 0.80
+############################################################
+
+
+
+library(huge)
+library(HMFGraph)
+library(qgraph)
+
+set.seed(42)
+n <-100 # number of samples
+p <-20 # number of variables
+graph_data <- huge.generator(n = n, d =p, vis = TRUE, graph ="scale-free")
+data_R <-graph_data$data
+real_admat <-round(graph_data$omega,5)
+diag(real_admat) <- 0
+real_admat[abs(real_admat) > 0] <- 1
+
+colors <- brewer.pal(5, "Dark2")  
+
+#====================================
+# True network
+#====================================
+
+setEPS()
+postscript("beta_values_a_080_offdiagonals.eps", width = 10, height = 11)   # koko oletuksena tuumina
+
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+m <- matrix(c(1,1,1,1,2,2),nrow = 3,ncol = 2,byrow = TRUE)
+layout(mat = m,heights = c(0.5,0.5,0.05))
+
+
+#====================================
+# beta = 0.95
+#====================================
+
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha =0.8, beta=0.95)
+
+plot(density(results_GEM$omega[lower.tri(graph_data$omega)])  , col=colors[1], lty =1, xlim=c(-0.15,0.25)
+     , cex.lab = 1.5,cex.main=2,cex.axis=1.5, lwd = 2
+     , ylab = "density", xlab="values of off-diagonal elements of estimated precision matrix", main="Distribution of off-diagonal elements in the precision matrix with multiple beta values")
+
+
+#====================================
+# beta=0.90
+#====================================
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha =0.8, beta=0.90)
+
+lines(density(results_GEM$omega[lower.tri(graph_data$omega)])  , col=colors[2], lty =2, lwd = 2,cex.lab = 1.2,)
+
+
+#====================================
+# beta=0.80
+#====================================
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha =0.8, beta=0.80)
+
+lines(density(results_GEM$omega[lower.tri(graph_data$omega)])  , col=colors[3], lty =3, lwd = 2,cex.lab = 1.2,)
+
+
+#====================================
+# beta=0.70
+#====================================
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha =0.8, beta=0.70)
+
+lines(density(results_GEM$omega[lower.tri(graph_data$omega)]) , col=colors[4], lty =4, lwd = 2,cex.lab = 1.2,)
+
+#====================================
+# beta=0.60
+#====================================
+results_GEM <- HMFGraph::HMFGraph_GEM(data_R, alpha =0.8, beta=0.60)
+
+lines(density(results_GEM$omega[lower.tri(graph_data$omega)]) , col=colors[5], lty =5, lwd = 2,cex.lab = 1.2,)
+
+par(mar = c(1, 1, 1, 1))
+plot(1, type = "n", axes=FALSE, xlab="", ylab="")
+plot_colors <- c(colors[1],colors[2], colors[3], colors[4], colors[5])
+
+legend(x = "top",inset = 0,
+       legend = c("beta = 0.95", "beta = 0.90", "beta = 0.80","beta = 0.70", "beta = 0.60"), 
+       col=plot_colors, lwd=2, cex=1.2, xpd = TRUE, horiz = TRUE, lty =c(1:5))
+
+dev.off()
